537503e5e539546a2d9826d5d84dcb23053dbcca
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-05-24 12:31:04 +0300

    add_birthday all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import readline
 from tabulate import tabulate
+from datetime import datetime, timedelta
 
 
 def add_contact():
@@ -27,20 +28,135 @@ def delete_contact():
     print("Feature coming soon")
 
 
-def add_birthday():
-    print("Feature coming soon")
+
+class User:
+    def __init__(self, id, name, birthday=None):
+        self.id = id
+        self.name = name
+        self.birthday = birthday  # День народження користувача у форматі DD-MM-YYYY
 
 
-def show_birthday():
-    print("Feature coming soon")
+class UsersDatabase:
+    def __init__(self):
+        self.users = []
+        self.next_id = 1
+        self.load_users()  # Завантаження користувачів з файлу при створенні об'єкта
 
+    # Функція для перевірки коректності дати народження
+    def validate_date(self, birthday_str):
+        try:
+            birthday = datetime.strptime(birthday_str, "%d-%m-%Y")
+            today = datetime.now().date()
+            # Перевірка, чи дата не є у майбутньому
+            if birthday.date() > today:
+                print("Дата народження не може бути у майбутньому.")
+                return False
+            # Перевірка, чи дата не більше за 120 років у минулому
+            if today - birthday.date() > timedelta(days=365 * 120):
+                print("Дата народження не може бути більше за 120 років у минулому.")
+                return False
+            return True
+        except ValueError:
+            print(
+                "Некоректний формат дати народження. Будь ласка, введіть у форматі 'DD-MM-YYYY'."
+            )
+            return False
 
-def show_all_birthdays():
-    print("Feature coming soon")
+    # Метод для додавання контакту
+    def add_birthday(self, user_id, new_birthday):
+        while not self.validate_date(new_birthday):
+            new_birthday = input(
+                "Введіть новий день народження у форматі 'DD-MM-YYYY': "
+            )
+        for user in self.users:
+            if user.id == user_id:
+                user.birthday = new_birthday
+                self.save_users()
+                print("День народження оновлено.")
+                return True
+        print("Користувача з вказаним ID не знайдено.")
+        return False
 
+    def save_users(self):
+        with open("users.json", "w") as file:
+            json_users = [
+                {"id": user.id, "name": user.name, "birthday": user.birthday}
+                for user in self.users
+            ]
+            json.dump(
+                json_users, file, indent=4
+            )  # Використовуйте indent для форматування JSON
 
-def search_by_date_birthday():
-    print("Feature coming soon")
+    # Метод для завантаження користувачів з файлу
+    def load_users(self):
+        try:
+            with open("users.json", "r") as file:
+                json_users = json.load(file)
+                self.users = [
+                    User(user["id"], user["name"], user["birthday"])
+                    for user in json_users
+                ]
+                self.next_id = (
+                    max(self.users, key=lambda user: user.id).id + 1
+                    if self.users
+                    else 1
+                )
+        except FileNotFoundError:
+            pass
+
+    # Метод для видалення дня народження користувача
+    def delete_birthday(self, user_id):
+        for user in self.users:
+            if user.id == user_id:
+                user.birthday = None  # Встановлюємо день народження користувача як None
+                self.save_users()
+                print("День народження користувача видалено.")
+                return True
+        print("Користувача з вказаним ID не знайдено.")
+        return False
+
+    # Метод для показу дня народження користувача
+    def show_birthday(self, user_id):
+        for user in self.users:
+            if user.id == user_id:
+                if user.birthday:
+                    # return str(user)  # Повертаємо користувача у вигляді рядка
+                    return user  # Повертаємо об'єкт користувача
+                else:
+                    return "День народження не встановлено для цього користувача."
+        return "Користувача з вказаним ID не знайдено."
+
+    # Метод для показу всіх днів народження
+    def show_all_birthdays(self):
+        birthdays = []
+        for user in self.users:
+            if user.birthday:
+                birthdays.append(
+                    f"ID: {user.id}, Name: {user.name}, Birthday: {user.birthday}"
+                )
+            else:
+                birthdays.append(
+                    f"ID: {user.id}, Name: {user.name}, Birthday: не встановлено"
+                )
+        return birthdays
+
+    # Метод для пошуку контактів за датою народження в заданому проміжку
+    def search_by_date_birthday(self, days):
+        today = datetime.now().date()
+        target_date = today + timedelta(days=days)
+        matching_users = []
+
+        for user in self.users:
+            if user.birthday:
+                # Перетворіть рядок з датою народження у об'єкт datetime.date
+                user_birthday = datetime.strptime(user.birthday, "%d-%m-%Y").date()
+                # Змініть рік дати народження на поточний
+                user_birthday_this_year = user_birthday.replace(year=today.year)
+                # Перевірте, чи дата народження відповідає заданому проміжку
+                if today <= user_birthday_this_year <= target_date:
+                    matching_users.append(user)
+
+        return matching_users
 
 
 # Клас для представлення нотатки з ідентифікатором, текстом та тегами
@@ -133,6 +249,7 @@ commands = {
     "all_contact": "Показати всі контакти",
     "delete_contact": "Видалити контакт",
     "add_birthday": "Додати день народження",
+    "delete_birthday": "Видалити день народження",  # додати видалити день народження
     "show_birthday": "Показати день народження",
     "show_all_birthdays": "Показати всі дні народження",
     "search_by_date_birthday": "Пошук за датою народження",
@@ -199,19 +316,20 @@ def print_commands(commands):
 # Головна функція програми
 def main():
     db = NotesDatabase()
+    users_db = UsersDatabase()
+
     print("\nAvailable commands:")
     print_commands(commands)
 
     while True:
 
-
         choice = get_command_input()
-
 
         # Обробка команд
         if choice == "hello":
             say_hello()
 
+        # Обробка команди "add_contact"
         elif choice == "add_contact":
             add_contact()
 
@@ -230,17 +348,89 @@ def main():
         elif choice == "delete_contact":
             delete_contact()
 
+        # Обробка команди "update_birthday" з перевіркою
         elif choice == "add_birthday":
-            add_birthday()
+            user_id_input = input(
+                "Введіть ID користувача, дату народження якого потрібно оновити: "
+            )
+            try:
+                user_id = int(user_id_input)
+                new_birthday = input(
+                    "Введіть новий день народження у форматі 'DD-MM-YYYY': "
+                )
+                users_db.add_birthday(user_id, new_birthday)
+            except ValueError:
+                print("ID користувача повинно бути цілим числом.")
 
+        # Обробка команди "delete_birthday"
+        elif choice == "delete_birthday":
+            while True:
+                user_id_input = input(
+                    "Введіть ID користувача, для якого потрібно видалити день народження: "
+                )
+                try:
+                    user_id = int(user_id_input)
+                    users_db.delete_birthday(user_id)
+                    break  # Вихід з циклу, якщо введення коректне
+                except ValueError:
+                    print(
+                        f"Неправильний ID користувача '{user_id_input}'. Будь ласка, введіть коректне ціле число."
+                    )
+
+        # Обробка команди "show_birthday"
         elif choice == "show_birthday":
-            show_birthday()
+            user_id_input = input(
+                "Введіть ID користувача, для якого потрібно показати день народження: "
+            )
+            try:
+                user_id = int(user_id_input)
+                user = users_db.show_birthday(user_id)
+                if user:
+                    print(
+                        f"ID: {user.id}, Name: {user.name}, Birthday: {user.birthday if user.birthday else 'не встановлено'}"
+                    )
+                else:
+                    print("Користувача з вказаним ID не знайдено.")
+            except ValueError:
+                print(
+                    f"Неправильний ID користувача '{user_id_input}'. Будь ласка, введіть коректне ціле число."
+                )
 
+        # Обробка команди "show_all_birthdays"
         elif choice == "show_all_birthdays":
-            show_all_birthdays()
+            birthdays = users_db.show_all_birthdays()
+            for birthday in birthdays:
+                print(birthday)
 
+        # Обробка команди "search_by_date_birthday"
         elif choice == "search_by_date_birthday":
-            search_by_date_birthday()
+            while True:
+                days_input = input(
+                    "Введіть кількість днів (максимум 365), на яку потрібно розширити проміжок для пошуку: "
+                )
+                try:
+                    days = int(days_input)
+                    if days > 365:
+                        print(
+                            "Кількість днів повинна бути не більше 365. Будь ласка, введіть коректне число."
+                        )
+                        continue  # Повернутись на початок циклу, щоб запитати введення знову
+                    matching_users = users_db.search_by_date_birthday(days)
+                    if matching_users:
+                        print(
+                            f"Контакти, у яких день народження відбувається в проміжку через {days} днів:"
+                        )
+                        for user in matching_users:
+                            print(
+                                f"ID: {user.id}, Name: {user.name}, Birthday: {user.birthday}"
+                            )
+                    else:
+                        print(
+                            f"Немає контактів, у яких день народження відбувається в проміжку через {days} днів."
+                        )
+                    break  # Вихід з циклу, якщо введення коректне
+                except ValueError:
+                    print("Неправильне значення днів. Будь ласка, введіть ціле число.")
 
         elif choice == "add_note":
             text = input("Enter note text: ")
@@ -286,7 +476,9 @@ def main():
             try:
                 note_id = int(note_id_input)
                 text = input("Enter new text for the note: ")
-                tags = input("Enter new tags for the note separated by comma: ").split(",")
+                tags = input("Enter new tags for the note separated by comma: ").split(
+                    ","
+                )
                 if db.update_note(note_id, text, tags):
                     print("Note updated.")
                     all_notes = db.get_all_notes()
